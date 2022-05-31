d1186f486d85674a8e71eae9fb7094fcecf7697f
Jan Boon <jan.boon@kaetemi.be>
2022-06-01 05:16:34 +0800

    Bump minor disk format

--- lfs.h
+++ lfs.h
@@ -30,7 +30,7 @@ extern "C"
 // Version of On-disk data structures
 // Major (top-nibble), incremented on backwards incompatible changes
 // Minor (bottom-nibble), incremented on feature additions
-#define LFS_DISK_VERSION 0x00020000
+#define LFS_DISK_VERSION 0x00020001
 #define LFS_DISK_VERSION_MAJOR (0xffff & (LFS_DISK_VERSION >> 16))
 #define LFS_DISK_VERSION_MINOR (0xffff & (LFS_DISK_VERSION >>  0))
 
